608220c641209020834d699cb35d3efa94847f16
Maciej Krzemianowski <dev@jmzmedia.eu>
2021-08-13 22:22:46 +0200

    feat: Publish menu state using webhook
    Closes #11

--- packages/pagexpress-server/models/SiteInfo.js
+++ packages/pagexpress-server/models/SiteInfo.js
@@ -1,22 +1,26 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 
-const siteInfo = new Schema({
+const siteInfoSchema = new Schema({
   url: { type: String, require: true, max: 250 },
   previewUrl: { type: String, max: 250 },
   imagesPath: { type: String, max: 250 },
+  publishWebhookURL: { type: String, max: 250 },
+  menuUpdateWebhookUrl: { type: String, max: 250 },
 });
 
 const siteInfoValidationSchema = Joi.object({
   url: Joi.string().required().max(250),
   previewUrl: Joi.string().max(250),
   imagesPath: Joi.string().max(250),
+  publishWebhookURL: Joi.string().max(250),
+  menuUpdateWebhookUrl: Joi.string().max(250),
 });
 
-const SiteInfo = model('SiteInfo', siteInfo);
+const SiteInfo = model('SiteInfo', siteInfoSchema);
 
 module.exports = {
   SiteInfo,
-  siteInfo,
+  siteInfoSchema,
   siteInfoValidationSchema,
 };
