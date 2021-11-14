da7f08981c162142a5627ab4dfa316e5d7954fd8
Maciej Krzemianowski <dev@jmzmedia.eu>
2021-11-14 15:09:10 +0100

    fix: Add missing rules in eslint/prettier configs

--- packages/pagexpress/.eslintrc.js
+++ packages/pagexpress/.eslintrc.js
@@ -10,19 +10,13 @@ module.exports = {
   extends: [
     '@nuxtjs',
     'prettier',
+    'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    'max-len': [
-      'error',
-      {
-        code: 80,
-        comments: 120,
-      },
-    ],
     'vue/html-self-closing': [
       'error',
       {
