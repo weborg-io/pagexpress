e76dd43bb4883f2fb02d88ec7f536fd742a34f18
Maciej Krzemianowski <dev@weborg.io>
2021-12-03 18:11:36 +0100

    hotfix: Add missing down method to migration

--- packages/pagexpress-server/migrations/20211203134719-rename-version-of-pagedetails.js
+++ packages/pagexpress-server/migrations/20211203134719-rename-version-of-pagedetails.js
@@ -5,7 +5,7 @@ module.exports = {
       .updateMany({ version: { $exists: false } }, [{ $set: { version: 1 } }, { $unset: ['__v'] }]);
   },
 
-  async up(db) {
+  async down(db) {
     await db
       .collection('pagedetails')
       .updateMany({ version: { $exists: true } }, [{ $set: { __v: 1 } }, { $unset: ['version'] }]);
