61589f59290b1607684fb98ceba7094756f60e0c
Maciej Krzemianowski <dev@weborg.io>
2021-12-03 16:24:31 +0100

    update: Set versionKey as "version"
    - Removed pre hook on "findOneAndUpdate" event

--- packages/pagexpress-server/models/PageDetails.js
+++ packages/pagexpress-server/models/PageDetails.js
@@ -25,26 +25,9 @@ const pageDetailsSchema = new Schema(
   {
     timestamps: true,
     optimisticConcurrency: true,
+    versionKey: 'version',
   }
 );
-
-pageDetailsSchema.pre('findOneAndUpdate', function() {
-  const update = this.getUpdate();
-  if (update.__v != null) {
-    delete update.__v;
-  }
-  const keys = ['$set', '$setOnInsert'];
-  for (const key of keys) {
-    if (update[key] != null && update[key].__v != null) {
-      delete update[key].__v;
-      if (Object.keys(update[key]).length === 0) {
-        delete update[key];
-      }
-    }
-  }
-  update.$inc = update.$inc || {};
-  update.$inc.__v = 1;
-});
 
 const pageComponentValidationSchema = Joi.object({
   _id: Joi.string().min(36).max(36),
