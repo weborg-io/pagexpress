5629e6b0720a0af51cc98b999c96e3257acfc6fd
Maciej Krzemianowski <dev@weborg.io>
2021-12-05 19:04:33 +0100

    update: WebSockets part code style improvement

---
+++ packages/pagexpress-server/controllers/rtm/rtm-page-details-controller.js
@@ -0,0 +1,17 @@
+/**
+ * @param {WebSocket} socket
+ */
+module.exports = socket => {
+  socket.on('editing-page-details', user =>
+    socket.broadcast.emit('editing-page-details', user)
+  );
+  socket.on('who-page-details', eventData =>
+    socket.broadcast.emit('who-page-details', eventData)
+  );
+  socket.on('left-page-details', eventData =>
+    socket.broadcast.emit('left-page-details', eventData)
+  );
+  socket.on('update-page-structure', version =>
+    socket.broadcast.emit('update-page-structure', version)
+  );
+};
--- packages/pagexpress-server/modules/server.js
+++ packages/pagexpress-server/modules/server.js
@@ -47,20 +47,7 @@ class Server {
     });
     this.app.io = io;
 
-    io.on('connection', socket => {
-      socket.on('editing-page-details', user =>
-        socket.broadcast.emit('editing-page-details', user)
-      );
-      socket.on('who-page-details', eventData =>
-        socket.broadcast.emit('who-page-details', eventData)
-      );
-      socket.on('left-page-details', eventData =>
-        socket.broadcast.emit('left-page-details', eventData)
-      );
-      socket.on('update-page-structure', version =>
-        socket.broadcast.emit('update-page-structure', version)
-      );
-    });
+    io.on('connection', require('../controllers/rtm'));
   }
 
   init() {
