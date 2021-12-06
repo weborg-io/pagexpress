d2f339cba26e0bec28206e679c44f26681c5d225
Maciej Krzemianowski <dev@weborg.io>
2021-12-06 22:38:36 +0100

    update: Change transports option to websocket

--- packages/pagexpress/plugins/socket.js
+++ packages/pagexpress/plugins/socket.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 export default ({ $config }, inject) => {
   const socket = io($config.websocketServerUrl, {
     reconnectionDelayMax: 10000,
+    transports: ['websocket'],
   });
   inject('socket', socket);
 };
